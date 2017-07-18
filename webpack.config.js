13effc59db0910c7bd946a9d834715beb0effc1c
pawel lewoniewski <pawel.lewoniewski@gmail.com>
2017-07-18 16:22:35 +0200

    structure

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const webpack = require('webpack');
 
 const extractSass = new ExtractTextPlugin({
     filename: "style.css",
-  //  disable: process.env.NODE_ENV === "development"
+    //  disable: process.env.NODE_ENV === "development"
 });
 
 //what give this starter: 
@@ -16,88 +16,84 @@ const extractSass = new ExtractTextPlugin({
 
 module.exports = {
     entry: {
-    app: './src/app.js' , 
-   // newpage:'./src/newpage.js'
+        app: './src/app.js',
+        // newpage:'./src/newpage.js'
     },
-    output: {    
+    output: {
         filename: '[name].bundle.js',
-        path: __dirname + '/dist' 
+        path: __dirname + '/dist'
     },
-    
+
 
     // css + sass modlules 
-    module:{
-          rules: [
-              {
-               test: /\.css$/,
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
                 use: [
                     'handlebars-loader', // handlebars loader expects raw resource string
                     'extract-loader',
                     'css-loader'
-                ]},
-              {  
-            test: /\.scss$/,
-            use: ['style-loader','css-loader','sass-loader'] 
-              },
-         {
-         test: /\.(png|svg|jpg|gif)$/,
-        use: [
-            'file-loader?name=[name].[ext]&outputPath=img/',
-            'image-webpack-loader'
-        ]
-       },
+                ]
+            },
+
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader',],
+                })
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader?name=[name].[ext]&outputPath=img/',
+                    'image-webpack-loader'
+                ]
+            },
         ]
     },
 
-    //dev server option like: open: new window 
-    devServer:{
-            contentBase: path.join(__dirname, "/dist"),
-            compress: true,
-            // open true/false opening new window in every start dev mode
-            open: true,
-            // hot dinamic css 
-             hot: true,
-             //showing only errors in console
-            stats: "errors-only",
+    //dev server option like: open: new w indow 
+    devServer: {
+        contentBase: path.join(__dirname, "/dist"),
+        compress: true,
+        // open true/false opening new window in every start dev mode
+        open: true,
+        // hot dinamic css 
+        hot: true,
+        //showing only errors in console
+        stats: "errors-only",
     },
-// plugins define new html webside , can add new to grow up project 
+    // plugins define new html webside , can add new to grow up project 
     plugins: [
         //this is a first site 
         new HtmlWebpackPlugin({
-        title:'new project',
-        minify:{
-            collapseWhitespace: true
-        },
-        // exclude - this turns off app.js for this page
-        hash: true,
-        excludeChunks: ['newpage'],
-        template:'./src/index.html',
-    }),
+            title: 'new project',
+            minify: {
+                collapseWhitespace: true
+            },
+            // exclude - this turns off app.js for this page
+            hash: true,
+            excludeChunks: ['newpage'],
+            template: './src/index.html',
+        }),
 
-    //next site and this turns off js for this website by using excludeChunks/chunks
-    // new HtmlWebpackPlugin({
-    //     title:'pagetwo',
-    //    hash: true,
-    //    chunks:['newpage'],
-    //     filename: 'newpage.html',
-    //     template:'./src/newpage.html'
-    // }),
+        // in new version webpack 2.0.0+
+        // new ExtractTextPlugin({
+        //     filename: (getPath) => {
+        //         return getPath('css/[name].css').replace('css/js', 'css');
+        //     },
+        //     allChunks: true
+        // }),
 
-    // in new version webpack 2.0.0+
-    new ExtractTextPlugin({
-        filename: (getPath) => {
-            return getPath('css/[name].css').replace('css/js', 'css');
-        },
-        allChunks: true
-    }),
-
-       new ExtractTextPlugin({
-        filename: '[name].bundle.css',
-        allChunks: true,
-       // disable: env === 'dev',
-      }),
-     new webpack.HotModuleReplacementPlugin() // Enable HMR
+        new ExtractTextPlugin({
+            filename: '[name].bundle.css',
+            allChunks: true,
+            disable: false,
+        }),
+        new webpack.HotModuleReplacementPlugin(), // Enable HMR
     // add sass
-   // extractSass
+    //extractSass
     ],
 }
