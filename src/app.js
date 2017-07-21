c8d62b4cc78fc5f9eee5195ee76385b2922fe01d
pawel lewoniewski <pawel.lewoniewski@gmail.com>
2017-07-21 11:50:12 +0200

    bezslidera

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
 
 import css from './app.scss';
 console.log("this is from app.js comming, like a winter in Brasil");
+
+
+var btn = document.querySelector('[data-js="btnnav"]');
+
+btn.addEventListener('click', function () {
+    var cl = this.getAttribute('class');
+    if (cl === 'buttonnav') {
+        this.classList.add('open');
+    }
+    else {
+        this.classList.remove('open');
+    }
+});
